6607054b947e6b1e83171739f7df351268e32e78
Karl Bennett <where_1ts@yahoo.com>
2012-08-16 19:31:55 +0100

    Commented the Cell and Rule.

--- src/main/java/org/karlbennett/gameoflife/Cell.java
+++ src/main/java/org/karlbennett/gameoflife/Cell.java
@@ -5,12 +5,30 @@ import java.util.List;
 /**
  * User: karl
  * Date: 16/08/12
+ *
+ * This is a cell is used to make up a board for the Game of Life. It's neighbours, current and rule modified state can
+ * be retrieved.
  */
 public interface Cell<S> {
 
+    /**
+     * Get the current state of the cell before the games rules have been applied.
+     *
+     * @return the current state.
+     */
     public S currentState();
 
+    /**
+     * Get the next state of the cell after the rules have been applied.
+     *
+     * @return the next state.
+     */
     public S nextState();
 
+    /**
+     * Get all the cells that are neighbours to this cell.
+     *
+     * @return all the cells neighbours.
+     */
     public List<Cell<S>> neighbours();
 }
--- src/main/java/org/karlbennett/gameoflife/Rule.java
+++ src/main/java/org/karlbennett/gameoflife/Rule.java
@@ -3,8 +3,19 @@ package org.karlbennett.gameoflife;
 /**
  * User: karl
  * Date: 16/08/12
+ *
+ * This is a rule for the Game of Life, it can be applied to a cell.
+ *
+ * @type C - the type of {@see Cell} that this rule can be applied to.
+ * @type S - the type of state that this rule produces.
  */
 public interface Rule<C extends Cell, S> {
 
+    /**
+     * Apply this rule to the supplied <code>Cell</code> and return the produced state.
+     *
+     * @param cell - the <code>Cell</code> to apply the rule to.
+     * @return the state the rule produces.
+     */
     public S apply(C cell);
 }
