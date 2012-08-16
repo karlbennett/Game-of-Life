1d20e9994dfce46c3505186bcc27fbc9101386db
Karl Bennett <where_1ts@yahoo.com>
2012-08-16 18:12:36 +0100

    First commit for The Game of Life.

---
+++ src/main/java/org/karlbennett/gameoflife/GameOfLife.java
@@ -0,0 +1,10 @@
+package org.karlbennett.gameoflife;
+
+/**
+ * Game of Life
+ */
+public class GameOfLife {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/org/karlbennett/gameoflife/GameOfLifeTest.java
@@ -0,0 +1,14 @@
+package org.karlbennett.gameoflife;
+
+import org.junit.Test;
+
+/**
+ * User: karl
+ * Date: 16/08/12
+ */
+public class GameOfLifeTest {
+
+    @Test
+    public void testMain() throws Exception {
+    }
+}
