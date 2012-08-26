6bb6c37761bd5570d4f3095962fe0ed0d5e0dbb5
Karl Bennett <where_1ts@yahoo.com>
2012-08-26 15:29:07 +0100

    Updated the Cell class so that it should soon support coordinate neighbour lookups.

--- src/main/java/org/karlbennett/gameoflife/Cell.java
+++ src/main/java/org/karlbennett/gameoflife/Cell.java
@@ -1,7 +1,7 @@
 package org.karlbennett.gameoflife;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,11 +13,25 @@ import java.util.List;
  */
 public class Cell<S extends Comparable<S>, R extends Rule<S>> {
 
+    /**
+     * Calculate the number of neighbours the cell should have in relation to the number of dimensions the cell supports.
+     *
+     * @param dimensions - the number of supported dimensions e.g. x, y, z...
+     * @return the number of neighbours this could potentially have.
+     */
+    public static int neighbourNumber(int dimensions) {
+
+        return (int) Math.pow(3, dimensions) - 1;
+    }
+
+
     private final S state;
 
     private S nextState;
 
     private final List<R> rules;
+
+    private int dimensions;
 
     private List<Cell<S, R>> neighbours;
 
@@ -26,12 +40,26 @@ public class Cell<S extends Comparable<S>, R extends Rule<S>> {
      *
      * @param state      - the state for this cell.
      * @param rules      - the rules that should be applied to this cell.
-     * @param neighbours - the cells direct neighbouring.
+     * @param dimensions - the number of dimensions for this cell e.g. 1D, 2D, 3D.
      */
-    public Cell(S state, List<R> rules, List<Cell<S, R>> neighbours) {
+    public Cell(S state, List<R> rules, int dimensions) {
 
         this.state = state;
-        this.rules = null == rules ? null : Collections.unmodifiableList(rules);
+        this.rules = rules;
+        this.dimensions = dimensions;
+        this.neighbours = new ArrayList<Cell<S, R>>(Arrays.<Cell<S, R>>asList(new Cell[neighbourNumber(dimensions)]));
+    }
+
+    public Cell(S state, List<R> rules, int dimensions, List<Cell<S, R>> neighbours) {
+
+        if (neighbourNumber(dimensions) != neighbours.size()) {
+
+            throw new IllegalStateException("The number supplied neighbours is incorrect in relation to the supported dimension.");
+        }
+
+        this.state = state;
+        this.rules = rules;
+        this.dimensions = dimensions;
         this.neighbours = neighbours;
     }
 
@@ -88,5 +116,66 @@ public class Cell<S extends Comparable<S>, R extends Rule<S>> {
     void setNeighbours(List<Cell<S, R>> neighbours) {
 
         this.neighbours = neighbours;
+    }
+
+    /**
+     * Retrieve the neighbour at the supplied coordinates where this cell is (0,0).
+     *
+     * @param coordinates - the coordinates for the cells neighbour.
+     * @return the requested neighbour.
+     * @throws IllegalArgumentException  if the supplied are (0,0) because that is coordinate of this cell not a
+     *                                   neighbour. Or if two many dimensional coordinates are supplied e.g. if (0,1,2) is supplied for a 2D cell.
+     * @throws IndexOutOfBoundsException if any coordinate value greater than 2 is supplied.
+     */
+    public Cell<S, R> getNeighbour(int... coordinates) {
+
+        checkCoordinates(coordinates);
+
+        return null;
+    }
+
+    /**
+     * Set the neighbour at the supplied coordinates where this cell is (0,0).
+     *
+     * @param neighbour   - the cell to set as a new neighbour.
+     * @param coordinates - the coordinates for the new neighbour.
+     * @throws IllegalArgumentException  if the supplied are (0,0) because that is coordinate of this cell not a
+     *                                   neighbour. Or if two many dimensional coordinates are supplied e.g. if (0,1,2) is supplied for a 2D cell.
+     * @throws IndexOutOfBoundsException if any coordinate value greater than 2 is supplied.
+     */
+    public void setNeighbour(Cell<S, R> neighbour, int... coordinates) {
+
+        checkCoordinates(coordinates);
+
+    }
+
+
+    /**
+     * Utility method for checking the supplied coordinates to make sure they are valid.
+     *
+     * @param coordinates - the coordinates to check.
+     * @throws IllegalArgumentException  if the supplied are (0,0) because that is coordinate of this cell not a
+     *                                   neighbour. Or if two many dimensional coordinates are supplied e.g. if (0,1,2) is supplied for a 2D cell.
+     * @throws IndexOutOfBoundsException if any coordinate value greater than 2 is supplied.
+     */
+    private void checkCoordinates(int... coordinates) {
+
+        if (coordinates.length != dimensions) {
+
+            throw new IllegalArgumentException("The number of supplied coordinates is incorrect. Should be (" +
+                    dimensions + "), but was (" + coordinates.length + ").");
+        }
+
+        int sum = 0;
+
+        for (int c : coordinates) {
+
+            sum += c;
+
+            if (2 < c) throw new IndexOutOfBoundsException("The coordinate value (" + c + ") is too large.");
+        }
+
+        if (0 == sum)
+            throw new IllegalArgumentException("The cell at coordinate (0,0) is the current cell not a neighbour.");
     }
 }
--- src/test/java/org/karlbennett/gameoflife/CellTest.java
+++ src/test/java/org/karlbennett/gameoflife/CellTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -13,10 +14,52 @@ import static org.junit.Assert.*;
  */
 public class CellTest {
 
+    private static final Cell<Coordinates, Rule<Coordinates>> _n1_n1 = new Cell<Coordinates, Rule<Coordinates>>(
+            new Coordinates(-1, -1), null, 0);
+    private static final Cell<Coordinates, Rule<Coordinates>> _0_n1 = new Cell<Coordinates, Rule<Coordinates>>(
+            new Coordinates(0, -1), null, 0);
+    private static final Cell<Coordinates, Rule<Coordinates>> _1_n1 = new Cell<Coordinates, Rule<Coordinates>>(
+            new Coordinates(1, -1), null, 0);
+    private static final Cell<Coordinates, Rule<Coordinates>> _n1_0 = new Cell<Coordinates, Rule<Coordinates>>(
+            new Coordinates(-1, 0), null, 0);
+    private static final Cell<Coordinates, Rule<Coordinates>> _0_0 = new Cell<Coordinates, Rule<Coordinates>>(
+            new Coordinates(0, 0), null, 0);
+    private static final Cell<Coordinates, Rule<Coordinates>> _1_0 = new Cell<Coordinates, Rule<Coordinates>>(
+            new Coordinates(1, 0), null, 0);
+    private static final Cell<Coordinates, Rule<Coordinates>> _n1_1 = new Cell<Coordinates, Rule<Coordinates>>(
+            new Coordinates(-1, 1), null, 0);
+    private static final Cell<Coordinates, Rule<Coordinates>> _0_1 = new Cell<Coordinates, Rule<Coordinates>>(
+            new Coordinates(0, 1), null, 0);
+    private static final Cell<Coordinates, Rule<Coordinates>> _1_1 = new Cell<Coordinates, Rule<Coordinates>>(
+            new Coordinates(1, 1), null, 0);
+
+    private static final int index_n1_n1 = 0;
+    private static final int index_0_n1 = 1;
+    private static final int index_1_n1 = 2;
+    private static final int index_n1_0 = 3;
+    private static final int index_1_0 = 4;
+    private static final int index_n1_1 = 5;
+    private static final int index_0_1 = 6;
+    private static final int index_1_1 = 7;
+
+    private static final List<Cell<Coordinates, Rule<Coordinates>>> NEIGHBOURS = Collections.unmodifiableList(
+            Arrays.asList(_n1_n1, _0_n1, _1_n1, _n1_0, _1_0, _n1_1, _0_1, _1_1)
+    );
+
+
+    @Test
+    public void testNeighbourNumber() throws Exception {
+
+        assertEquals("neighbour number for 0 dimensions should be correct", 0, Cell.neighbourNumber(0));
+        assertEquals("neighbour number for 1 dimensions should be correct", 2, Cell.neighbourNumber(1));
+        assertEquals("neighbour number for 2 dimensions should be correct", 8, Cell.neighbourNumber(2));
+        assertEquals("neighbour number for 3 dimensions should be correct", 26, Cell.neighbourNumber(3));
+    }
+
     @Test
     public void testGetNextStateWithNullRules() throws Exception {
 
-        Cell<Boolean, Rule<Boolean>> cell = new Cell<Boolean, Rule<Boolean>>(true, null, null);
+        Cell<Boolean, Rule<Boolean>> cell = new Cell<Boolean, Rule<Boolean>>(true, null, 0);
 
         assertTrue("next state with no rules should be true", cell.getNextState());
     }
@@ -36,7 +79,7 @@ public class CellTest {
         Cell<Boolean, Rule<Boolean>> cell = new Cell<Boolean, Rule<Boolean>>(
                 true,
                 Collections.singletonList(rule),
-                null);
+                0);
 
         assertFalse("next state with one rule should be false", cell.getNextState());
     }
@@ -74,8 +117,167 @@ public class CellTest {
         Cell<Boolean, Rule<Boolean>> cell = new Cell<Boolean, Rule<Boolean>>(
                 true,
                 Arrays.asList(rule1, rule2, rule3),
-                null);
+                0);
 
         assertFalse("next state with multiple rules should be false", cell.getNextState());
+    }
+
+    @Test
+    public void testGetNeighbour() throws Exception {
+
+        Cell<Coordinates, Rule<Coordinates>> cell = new Cell<Coordinates, Rule<Coordinates>>(_0_0.getState(), null,
+                0);
+        cell.setNeighbours(NEIGHBOURS);
+
+        assertEquals("cell set to coordinate (-1,-1) should be  correct", _n1_n1.getState(), cell.getNeighbour(-1, -1).getState());
+        assertEquals("cell set to coordinate (0,-1) should be correct", _0_n1.getState(), cell.getNeighbour(0, -1).getState());
+        assertEquals("cell set to coordinate (1,-1) should be correct", _1_n1.getState(), cell.getNeighbour(1, -1).getState());
+        assertEquals("cell set to coordinate (-1,0) should be correct", _n1_0.getState(), cell.getNeighbour(-1, 0).getState());
+        assertEquals("cell set to coordinate (1,0) should be correct", _1_0.getState(), cell.getNeighbour(1, 0).getState());
+        assertEquals("cell set to coordinate (-1,1) should be correct", _n1_1.getState(), cell.getNeighbour(-1, 1).getState());
+        assertEquals("cell set to coordinate (0,1) should be correct", _0_1.getState(), cell.getNeighbour(0, 1).getState());
+        assertEquals("cell set to coordinate (1,1) should be correct", _1_1.getState(), cell.getNeighbour(1, 1).getState());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetNeighbourWithInvalidCoordinates() throws Exception {
+
+        Cell<Coordinates, Rule<Coordinates>> cell = new Cell<Coordinates, Rule<Coordinates>>(
+                _0_0.getState(), null, 2);
+
+        cell.getNeighbour(0, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetNeighbourWithTooManyDimensions() throws Exception {
+
+        Cell<Coordinates, Rule<Coordinates>> cell = new Cell<Coordinates, Rule<Coordinates>>(
+                _0_0.getState(), null, 2);
+
+        cell.getNeighbour(0, 1, 2);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetNeighbourWithTooHighCoordinates() throws Exception {
+
+        Cell<Coordinates, Rule<Coordinates>> cell = new Cell<Coordinates, Rule<Coordinates>>(
+                _0_0.getState(), null, 2);
+
+        cell.getNeighbour(0, 3);
+    }
+
+    @Test
+    public void testSetNeighbour() throws Exception {
+
+        Cell<Coordinates, Rule<Coordinates>> cell = new Cell<Coordinates, Rule<Coordinates>>(
+                _0_0.getState(), null, 2);
+
+        cell.setNeighbour(_n1_n1, -1, -1);
+        assertEquals("cell at coordinate (-1,-1) should be  correct", _n1_n1.getState(),
+                cell.getNeighbours().get(index_n1_n1));
+
+        cell.setNeighbour(_0_n1, 0, -1);
+        assertEquals("cell at coordinate (0,-1) should be  correct", _0_n1.getState(),
+                cell.getNeighbours().get(index_0_n1));
+
+        cell.setNeighbour(_1_n1, 1, -1);
+        assertEquals("cell at coordinate (1,-1) should be  correct", _1_n1.getState(),
+                cell.getNeighbours().get(index_1_n1));
+
+        cell.setNeighbour(_n1_0, -1, 0);
+        assertEquals("cell at coordinate (-1,0) should be  correct", _n1_0.getState(),
+                cell.getNeighbours().get(index_n1_0));
+
+        cell.setNeighbour(_1_0, 1, 0);
+        assertEquals("cell at coordinate (1,0) should be  correct", _1_0.getState(),
+                cell.getNeighbours().get(index_1_0));
+
+        cell.setNeighbour(_n1_1, -1, 1);
+        assertEquals("cell at coordinate (-1,1) should be  correct", _n1_1.getState(),
+                cell.getNeighbours().get(index_n1_1));
+
+        cell.setNeighbour(_0_1, 0, 1);
+        assertEquals("cell at coordinate (0,1) should be  correct", _0_1.getState(),
+                cell.getNeighbours().get(index_0_1));
+
+        cell.setNeighbour(_1_1, 1, 1);
+        assertEquals("cell at coordinate (1,1) should be  correct", _1_1.getState(),
+                cell.getNeighbours().get(index_1_1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetNeighbourWithInvalidCoordinates() throws Exception {
+
+        Cell<Coordinates, Rule<Coordinates>> cell = new Cell<Coordinates, Rule<Coordinates>>(
+                _0_0.getState(), null, 2);
+
+        cell.setNeighbour(null, 0, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetNeighbourWithTooManyDimensions() throws Exception {
+
+        Cell<Coordinates, Rule<Coordinates>> cell = new Cell<Coordinates, Rule<Coordinates>>(
+                _0_0.getState(), null, 2);
+
+        cell.setNeighbour(null, 0, 1, 2);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSetNeighbourWithTooHighCoordinates() throws Exception {
+
+        Cell<Coordinates, Rule<Coordinates>> cell = new Cell<Coordinates, Rule<Coordinates>>(
+                _0_0.getState(), null, 2);
+
+        cell.setNeighbour(null, 0, 3);
+    }
+
+    private static class Coordinates implements Comparable<Coordinates> {
+
+        public final int[] coordinates;
+
+
+        private Coordinates(int ...coordinates) {
+
+            this.coordinates = coordinates;
+        }
+
+        @Override
+        public int compareTo(Coordinates other) {
+
+            // If the supplied coordinates have more dimensions then they are greater than the current ones.
+            if (this.coordinates.length < other.coordinates.length) return -1;
+
+            // If the supplied coordinates have less dimensions then they are less than the current ones.
+            if (this.coordinates.length > other.coordinates.length) return -1;
+
+            int total = 0;
+            int othertotal = 0;
+            for (int i = 0; i < this.coordinates.length; i++) {
+
+                total += this.coordinates[i] * (Math.pow(10, i));
+                othertotal += other.coordinates[i] * (Math.pow(10, i));
+            }
+
+            return total - othertotal;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+
+            if (this == other) return true;
+
+            if (other == null || getClass() != other.getClass()) return false;
+
+            Coordinates that = (Coordinates) other;
+
+            return Arrays.equals(coordinates, that.coordinates);
+        }
+
+        @Override
+        public int hashCode() {
+
+            return Arrays.hashCode(coordinates);
+        }
     }
 }
